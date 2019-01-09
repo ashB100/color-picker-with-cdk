38a6a747b0391d496f1d91c4a9932b4b3024ce45
Ashnita Bali <ashnita01@gmail.com>
2019-01-09 20:24:42 +0000

    Animate color picker open and close

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { ColorPickerTriggerDirective } from './color-picker/color-picker-trigger.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ColorPickerTriggerDirective } from './color-picker/color-picker-trigger
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     OverlayModule,
     AppRoutingModule
   ],
--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -1,9 +1,26 @@
 import { Component, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-color-picker',
   templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.scss']
+  styleUrls: ['./color-picker.component.scss'],
+  // animation will expand, contract, and fade in depending on
+  // whether the datepicker is open or closed
+  animations: [
+    trigger('picker', [
+      state('void', style({
+        transform: 'scale(0)',
+        opacity: 0
+      })),
+      transition('void <=> *', [
+        style({
+          opacity: 1
+        }),
+        animate('550ms cubic-bezier(0.25, 0.8, 0.25, 1)')
+      ])
+    ])
+  ]
 })
 export class ColorPickerComponent {
   @Input() colors: object[];
